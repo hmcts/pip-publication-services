c9f19f35cf724cac0e9283f2d44b12b5d011308c
Christopher Pateman <christopher.pateman@version1.com>
2021-10-20 10:42:18 +0100

    correct logging and enable build notification

--- src/test/java/uk/gov/hmcts/reform/pip/publication/services/client/EmailClientTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/publication/services/client/EmailClientTest.java
@@ -1,13 +1,13 @@
 package uk.gov.hmcts.reform.pip.publication.services.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
