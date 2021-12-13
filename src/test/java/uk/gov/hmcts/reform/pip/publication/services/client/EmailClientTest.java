5ab9268faf2aea4a05ba41ae148bdfc8ee6a5200
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-12-13 16:50:43 +0000

    Fixed issue with spring test

--- src/test/java/uk/gov/hmcts/reform/pip/publication/services/client/EmailClientTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/publication/services/client/EmailClientTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -11,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application.yaml")
+@ActiveProfiles("test")
 class EmailClientTest {
 
     private static final String API_KEY = "7033ceaf-4fdf-4c72-84a8-d916762ddbeb";
