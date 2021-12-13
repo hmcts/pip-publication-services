d02df23b51701ac6a647358afb54548a341227df
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2021-12-13 16:53:19 +0000

    Fixed checkstyles

--- src/test/java/uk/gov/hmcts/reform/pip/publication/services/client/EmailClientTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/publication/services/client/EmailClientTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
