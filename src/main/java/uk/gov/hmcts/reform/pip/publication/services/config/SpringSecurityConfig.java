aa036c474a63164ed17a594aec0c6478b9f2bb41
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-04-26 18:14:40 +0100

    Added values + dev profile

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/config/SpringSecurityConfig.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/config/SpringSecurityConfig.java
@@ -1,12 +1,14 @@
 package uk.gov.hmcts.reform.pip.publication.services.config;
 
 import com.azure.spring.cloud.autoconfigure.aad.AadResourceServerWebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Profile("!dev")
 @SuppressWarnings("java:S4502")
 public class SpringSecurityConfig extends AadResourceServerWebSecurityConfigurerAdapter {
 
