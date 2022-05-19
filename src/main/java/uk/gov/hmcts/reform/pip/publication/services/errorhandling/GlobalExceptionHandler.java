7cee9405e5a0ad79f1de8d42b2ebf3d8a34ae3b8
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-05-20 00:10:34 +0100

    Updated values, supressions and global exception handler

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/errorhandling/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/errorhandling/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.pip.publication.services.errorhandling;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import uk.gov.hmcts.reform.pip.publication.services.errorhandling.exceptions.BadPayloadException;
@@ -42,6 +43,18 @@ public class GlobalExceptionHandler {
 
         ExceptionResponse exceptionResponse = new ExceptionResponse();
         exceptionResponse.setMessage(ex.getMessage());
+        exceptionResponse.setTimestamp(LocalDateTime.now());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handle(MethodArgumentNotValidException ex) {
+
+        log.error(String.format("MethodArgumentNotValidException was thrown with the init cause: %s", ex.getCause()));
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        exceptionResponse.setMessage(ex.getAllErrors().get(0).getDefaultMessage());
         exceptionResponse.setTimestamp(LocalDateTime.now());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exceptionResponse);
--- src/test/java/uk/gov/hmcts/reform/pip/publication/services/errorhandling/GlobalExceptionHandlerTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/publication/services/errorhandling/GlobalExceptionHandlerTest.java
@@ -1,21 +1,41 @@
 package uk.gov.hmcts.reform.pip.publication.services.errorhandling;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import uk.gov.hmcts.reform.pip.publication.services.errorhandling.exceptions.BadPayloadException;
 import uk.gov.hmcts.reform.pip.publication.services.errorhandling.exceptions.NotifyException;
 import uk.gov.hmcts.reform.pip.publication.services.errorhandling.exceptions.PublicationNotFoundException;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class GlobalExceptionHandlerTest {
 
     static final String TEST_MESSAGE = "This is a test message";
     static final String STATUS_CODE = "Status code should be not found";
     static final String BODY_RESPONSE = "Response should contain a body";
     static final String PASSED_IN_MESSAGE = "The message should match the message passed in";
+
+    @Mock
+    MethodArgumentNotValidException methodArgumentNotValidException;
+
+    @BeforeAll
+    public static void setup() {
+        MockitoAnnotations.openMocks(MethodArgumentNotValidException.class);
+    }
 
     @Test
     void testHandleSubscriptionNotFound() {
@@ -78,5 +98,23 @@ class GlobalExceptionHandlerTest {
         assertNotNull(responseEntity.getBody(), BODY_RESPONSE);
         assertEquals(TEST_MESSAGE, responseEntity.getBody().getMessage(),
                      PASSED_IN_MESSAGE);
+    }
+
+    @Test
+    void testMethodArgumentTypeMismatchException() {
+        GlobalExceptionHandler globalExceptionHandler = new GlobalExceptionHandler();
+
+        when(methodArgumentNotValidException.getAllErrors()).thenReturn(List.of(
+            new ObjectError("Subscription", "Error message")));
+
+        ResponseEntity<ExceptionResponse> responseEntity =
+            globalExceptionHandler.handle(methodArgumentNotValidException);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode(), STATUS_CODE);
+        assertNotNull(responseEntity.getBody(), BODY_RESPONSE);
+
+        assertTrue(responseEntity.getBody().getMessage().contains("Error message"),
+                   "Body does not contain error message");
+
     }
 }
