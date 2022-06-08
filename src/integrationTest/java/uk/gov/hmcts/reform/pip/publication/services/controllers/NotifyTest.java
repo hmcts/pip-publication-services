aac535992d6e714ad04d995ef04aacb6e2b5d471
Kurt Plaschke <k.plaschke@hull.ac.uk>
2022-06-08 15:04:25 +0100

    fixed checkstyle issue

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/controllers/NotifyTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/controllers/NotifyTest.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.reform.pip.publication.services.Application;
+
 import java.io.IOException;
 
 import static okhttp3.tls.internal.TlsUtil.localhost;
