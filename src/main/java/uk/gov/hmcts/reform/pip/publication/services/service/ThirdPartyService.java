6c8873a0660e0cfe28e558f5a9adc9fa3a1f5214
Kurt Plaschke <k.plaschke@hull.ac.uk>
2022-06-14 11:20:44 +0100

    added config for retries and backoff

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/ThirdPartyService.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/ThirdPartyService.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.pip.publication.services.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.util.retry.Retry;
@@ -12,6 +13,12 @@ import java.time.Duration;
 @Service
 @Slf4j
 public class ThirdPartyService {
+
+    @Value("${error-handling.num-of-retries}")
+    private int numOfRetries;
+
+    @Value("${error-handling.backoff}")
+    private int backoff;
 
     private static final String SUCCESS_MESSAGE = "Successfully sent list to %s at: %s";
     private static final String COURTEL = "Courtel";
@@ -24,9 +31,9 @@ public class ThirdPartyService {
             .bodyValue(payload)
             .retrieve()
             .bodyToMono(Void.class)
-            .retryWhen(Retry.backoff(3, Duration.ofSeconds(2))
+            .retryWhen(Retry.backoff(numOfRetries, Duration.ofSeconds(backoff))
                            .doAfterRetry(signal -> log.info(
-                               "Request failed, retrying {}/3",
+                               "Request failed, retrying {}/" + numOfRetries,
                                signal.totalRetries() + 1
                            ))
                            .onRetryExhaustedThrow((retryBackoffSpec, retrySignal) ->
