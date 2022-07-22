ee6e2775e55968bd6378202ee8e04201cf2f9f96
Daniel Furnivall <danielfurnivall@gmail.com>
2022-07-22 15:58:54 +0100

    test class covering potentially both the helper and models as well as converter class - let's see

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/pdf/converters/SjpPressListConverter.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/pdf/converters/SjpPressListConverter.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.pip.publication.services.service.pdf.converters;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import uk.gov.hmcts.reform.pip.publication.services.config.ThymeleafConfiguration;
@@ -14,20 +15,33 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+/**
+ * Converter class for SJP Press Lists - builds a nice pdf from input json and an html template (found in
+ * resources/mocks). Uses Thymeleaf to take in variables from model and build appropriately. Final output string is
+ * passed in to PDF Creation Service.
+ */
 @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
+@Service
 public class SjpPressListConverter implements Converter {
 
     Helpers helpers = new Helpers();
 
     public static final String INDIVIDUAL_DETAILS = "individualDetails";
 
+    /**
+     * parent method for the process.
+     * @param jsonBody - JsonNode representing the data within our jsonBody.
+     * @param metadata - immutable map containing relevant data from request headers (i.e. not within json body) used
+     *                to inform the template
+     * @return - html string of final output
+     */
     @Override
-    public String convert(JsonNode artefact, Map<String, String> metadata) {
+    public String convert(JsonNode jsonBody, Map<String, String> metadata) {
         Context context = new Context();
         List<SjpPressList> caseList = new ArrayList<>();
         int count = 1;
         Iterator<JsonNode> hearingNode =
-            artefact.get("courtLists").get(0).get("courtHouse").get("courtRoom").get(0).get("session").get(0).get(
+            jsonBody.get("courtLists").get(0).get("courtHouse").get("courtRoom").get(0).get("session").get(0).get(
                 "sittings").get(0).get("hearing").elements();
         while (hearingNode.hasNext()) {
             JsonNode currentCase = hearingNode.next();
@@ -41,7 +55,7 @@ public class SjpPressListConverter implements Converter {
         }
 
         String publishedDate = helpers.formatTimestampToBst(
-            artefact.get("document").get("publicationDate").asText(),
+            jsonBody.get("document").get("publicationDate").asText(),
             false
         );
         context.setVariable("contentDate", helpers.formatTimestampToBst(
@@ -49,40 +63,57 @@ public class SjpPressListConverter implements Converter {
             true
         ));
         context.setVariable("publishedDate", publishedDate);
-        context.setVariable("jsonBody", artefact);
+        context.setVariable("jsonBody", jsonBody);
         context.setVariable("metaData", metadata);
         context.setVariable("cases", caseList);
-        context.setVariable("artefact", artefact);
+        context.setVariable("artefact", jsonBody);
         SpringTemplateEngine templateEngine = new ThymeleafConfiguration().templateEngine();
         return templateEngine.process("sjpPressList.html", context);
     }
 
-    void processRoles(SjpPressList currentCase, JsonNode party) {
+    /**
+     * method for handling roles - sorts out accused and prosecutor roles and grabs relevant data from the json body.
+     * @param thisCase - case model which is updated by the method.
+     * @param party - node to be parsed.
+     */
+    void processRoles(SjpPressList thisCase, JsonNode party) {
         Iterator<JsonNode> partyNode = party.get("party").elements();
         while (partyNode.hasNext()) {
             JsonNode currentParty = partyNode.next();
             if ("accused".equals(currentParty.get("partyRole").asText().toLowerCase(Locale.ROOT))) {
                 JsonNode individualDetailsNode = currentParty.get(INDIVIDUAL_DETAILS);
-                currentCase.setName(individualDetailsNode.get("individualForenames").asText() + " "
+                thisCase.setName(individualDetailsNode.get("individualForenames").asText() + " "
                                         + individualDetailsNode.get("individualSurname").asText());
-                processAddress(currentCase, individualDetailsNode.get("address"));
-                currentCase.setDateOfBirth(individualDetailsNode.get("dateOfBirth").asText());
-                currentCase.setAge(individualDetailsNode.get("age").asText());
+                processAddress(thisCase, individualDetailsNode.get("address"));
+                thisCase.setDateOfBirth(individualDetailsNode.get("dateOfBirth").asText());
+                thisCase.setAge(individualDetailsNode.get("age").asText());
             } else {
-                currentCase.setProsecutor(currentParty.get("organisationDetails").get("organisationName").asText());
+                thisCase.setProsecutor(currentParty.get("organisationDetails").get("organisationName").asText());
             }
         }
     }
 
-    void processCaseUrns(SjpPressList currentCaseNode, JsonNode caseNode) {
+    /**
+     * case URN processing method. Takes in a case and a case node and grabs all case urns. It is worth mentioning
+     * that currently the case Urn field cannot safely be linked to offences where there are more
+     * than one as they are on a different level of the json hierarchy.
+     * @param thisCase - model representing case data.
+     * @param caseNode - json node containing cases on given case.
+     */
+    void processCaseUrns(SjpPressList thisCase, JsonNode caseNode) {
         List<String> caseUrns = new ArrayList<>();
         for (final JsonNode currentCase : caseNode) {
             caseUrns.add(currentCase.get("caseUrn").asText());
         }
-        currentCaseNode.setReference1(caseUrns.get(0));
-        currentCaseNode.setReferenceRemainder(caseUrns.stream().skip(1).collect(Collectors.toList()));
+        thisCase.setReference1(caseUrns.get(0));
+        thisCase.setReferenceRemainder(caseUrns.stream().skip(1).collect(Collectors.toList()));
     }
 
+    /**
+     * Handling address lines for the model.
+     * @param thisCase - our case model.
+     * @param addressNode - our node containing address data.
+     */
     void processAddress(SjpPressList thisCase, JsonNode addressNode) {
         List<String> address = new ArrayList<>();
         JsonNode lineArray = addressNode.get("line");
@@ -101,7 +132,11 @@ public class SjpPressListConverter implements Converter {
                                          .collect(Collectors.toList()));
     }
 
-
+    /**
+     * Method which populates the offence list in our case model.
+     * @param currentCase - case model.
+     * @param offencesNode - node containing our offence data.
+     */
     void processOffences(SjpPressList currentCase, JsonNode offencesNode) {
         List<Map<String, String>> listOfOffences = new ArrayList<>();
         Iterator<JsonNode> offences = offencesNode.elements();
@@ -118,6 +153,11 @@ public class SjpPressListConverter implements Converter {
         currentCase.setOffences(listOfOffences);
     }
 
+    /**
+     * Method to apply reporting restriction text to our model.
+     * @param node - json node representing an offence.
+     * @return a String containing the relevant text based on reporting restriction.
+     */
     private String processReportingRestrictionsjpPress(JsonNode node) {
         boolean restriction = node.get("reportingRestriction").asBoolean();
         if (restriction) {
---
+++ src/test/java/uk/gov/hmcts/reform/pip/publication/services/service/pdf/converters/SjpPressListConverterTest.java
@@ -0,0 +1,58 @@
+package uk.gov.hmcts.reform.pip.publication.services.service.pdf.converters;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import uk.gov.hmcts.reform.pip.publication.services.service.pdf.helpers.Helpers;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class SjpPressListConverterTest {
+
+    @Autowired
+    SjpPressListConverter sjpPressListConverter;
+
+    @Autowired
+    Helpers helpers;
+
+    @Test
+    void testSjpPressListTemplate() throws IOException {
+        StringWriter writer = new StringWriter();
+        IOUtils.copy(Files.newInputStream(Paths.get("src/test/resources/mocks/", "sjpPressMockJul22.json")), writer,
+                     Charset.defaultCharset()
+        );
+        Map<String, String> metadataMap = Map.of("contentDate", Instant.now().toString(),
+                                                 "provenance", "provenance",
+                                                 "location", "location"
+        );
+
+        JsonNode inputJson = new ObjectMapper().readTree(writer.toString());
+        String outputHtml = sjpPressListConverter.convert(inputJson, metadataMap);
+        Document document = Jsoup.parse(outputHtml);
+        assertThat(outputHtml).as("No html found").isNotEmpty();
+        assertThat(document.title()).as("incorrect title found.")
+            .isEqualTo("Single Justice Procedure - Press List "
+                           + helpers.formatTimestampToBst(metadataMap.get("contentDate"), true));
+
+        assertThat(document.getElementsByClass("mainHeaderText")
+                       .select(".mainHeaderText > h1:nth-child(1)").text())
+            .as("incorrect header text").isEqualTo("Single Justice Procedure Press List");
+        assertThat(document.select("div.pageSeparatedCase:nth-child(1) > table:nth-child(3) > tbody:nth-child(1) > "
+                                       + "tr:nth-child(6) > td:nth-child(2)").text())
+            .as("incorrect value found").isEqualTo("Hampshire Police");
+    }
+}
