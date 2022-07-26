9d45f976ab5c531258940ddfd4a81cbf42c4c8fa
Daniel Furnivall <danielfurnivall@gmail.com>
2022-07-26 18:50:48 +0100

    fixed tests and implemented Chris' suggestion of splitting into smaller classes

--- src/test/java/uk/gov/hmcts/reform/pip/publication/services/service/ArtefactSummaryServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/publication/services/service/ArtefactSummaryServiceTest.java
@@ -4,6 +4,7 @@ import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import uk.gov.hmcts.reform.pip.publication.services.models.external.ListType;
 import uk.gov.hmcts.reform.pip.publication.services.service.artefactsummary.ArtefactSummaryService;
 
@@ -16,6 +17,7 @@ import java.nio.file.Paths;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ArtefactSummaryServiceTest {
 
     @Autowired
