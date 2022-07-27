67554483f57a0976da395410a903bceffb3440c8
KianKwa <kian.kwa@justice.gov.uk>
2022-07-27 14:16:59 +0100

    Fixed checkstyle complaint

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/PdfCreationService.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/PdfCreationService.java
@@ -11,8 +11,8 @@ import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import uk.gov.hmcts.reform.pip.publication.services.config.ThymeleafConfiguration;
 import uk.gov.hmcts.reform.pip.publication.services.models.external.Artefact;
-import uk.gov.hmcts.reform.pip.publication.services.service.pdf.converters.Converter;
 import uk.gov.hmcts.reform.pip.publication.services.service.helpers.DateTimeHelper;
+import uk.gov.hmcts.reform.pip.publication.services.service.pdf.converters.Converter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
