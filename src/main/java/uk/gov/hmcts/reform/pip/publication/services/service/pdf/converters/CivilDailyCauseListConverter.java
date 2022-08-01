761ad644bfa007aad7bcdce855416164d005894c
KianKwa <kian.kwa@justice.gov.uk>
2022-08-01 16:13:56 +0100

    Fix sonarqube complaint on duplicated code

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/pdf/converters/CivilDailyCauseListConverter.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/pdf/converters/CivilDailyCauseListConverter.java
@@ -1,33 +1,19 @@
 package uk.gov.hmcts.reform.pip.publication.services.service.pdf.converters;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import uk.gov.hmcts.reform.pip.publication.services.config.ThymeleafConfiguration;
-import uk.gov.hmcts.reform.pip.publication.services.service.pdf.helpers.DataManipulation;
-import uk.gov.hmcts.reform.pip.publication.services.service.pdf.helpers.DateHelper;
 
 import java.util.Map;
+
+import static uk.gov.hmcts.reform.pip.publication.services.service.pdf.helpers.DailyCauseListHelper.preprocessArtefactForThymeLeafConverter;
 
 public class CivilDailyCauseListConverter implements Converter {
 
     @Override
     public String convert(JsonNode artefact, Map<String, String> artefactValues) {
-        Context context = new Context();
-        String publicationDate = artefact.get("document").get("publicationDate").asText();
-        DataManipulation.formatCourtAddress(artefact);
-
-        context.setVariable("publicationDate", DateHelper.formatTimeStampToBst(publicationDate, false, false));
-        context.setVariable("publicationTime", DateHelper.formatTimeStampToBst(publicationDate, true, false));
-        context.setVariable("contentDate", artefactValues.get("contentDate"));
-        context.setVariable("locationName", artefactValues.get("locationName"));
-        context.setVariable("provenance", artefactValues.get("provenance"));
-        context.setVariable("venueAddress", DataManipulation.formatVenueAddress(artefact));
-        context.setVariable("artefact", artefact);
-
-        DataManipulation.manipulatedDailyListData(artefact);
-
         SpringTemplateEngine templateEngine = new ThymeleafConfiguration().templateEngine();
-        return templateEngine.process("civilDailyCauseList.html", context);
+        return templateEngine.process("civilDailyCauseList.html",
+                                      preprocessArtefactForThymeLeafConverter(artefact, artefactValues));
     }
 }
