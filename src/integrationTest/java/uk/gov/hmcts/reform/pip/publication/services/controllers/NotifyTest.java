3e5b3f775a5cc797bc59f872553dc77d877abd12
Josh <joshblackmoor@users.noreply.github.com>
2022-09-08 11:13:13 +0100

    Get publication fix (#159)
    * Fix made
    
    * Updated tests
    
    Co-authored-by: junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/controllers/NotifyTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/controllers/NotifyTest.java
@@ -137,7 +137,7 @@ class NotifyTest {
         + SUBSCRIPTION_REQUEST;
 
     private static final String VALID_SJP_PRESS_SUBS_EMAIL = NEW_LINE_WITH_BRACKET
-        + "  \"artefactId\": \"41ab3903-c87c-42b5-994f-9b55f6dcad48\",\n"
+        + "  \"artefactId\": \"8a0ca51e-847e-4c6d-ad0f-a3f104f27845\",\n"
         + "  \"email\": \"test_account_admin@justice.gov.uk\",\n"
         + SUBSCRIPTION_REQUEST;
 
