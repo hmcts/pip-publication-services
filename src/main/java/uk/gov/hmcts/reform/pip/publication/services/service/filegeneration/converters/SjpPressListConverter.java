7a81d4d0d3db8b42080dec5f2bf2092be948f338
Josh Blackmoor <joshblackmoor01@icloud.com>
2022-09-21 18:47:21 +0100

    Fixed checkstyle

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/filegeneration/converters/SjpPressListConverter.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/filegeneration/converters/SjpPressListConverter.java
@@ -20,7 +20,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 /**
