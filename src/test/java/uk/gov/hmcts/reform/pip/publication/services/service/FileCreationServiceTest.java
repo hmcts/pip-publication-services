df5dbab941d46c173a2f78e877be042960f86c11
Daniel Furnivall <danielfurnivall@gmail.com>
2022-09-23 16:18:04 +0100

    new parameterized test

--- src/test/java/uk/gov/hmcts/reform/pip/publication/services/service/FileCreationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/publication/services/service/FileCreationServiceTest.java
@@ -26,6 +26,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -48,12 +49,73 @@ class FileCreationServiceTest {
     private static final List<MediaApplication> MEDIA_APPLICATION_LIST = List.of(new MediaApplication(
         UUID.randomUUID(), "Test user", "test@email.com", "Test employer",
         UUID.randomUUID().toString(), "test-image.png", LocalDateTime.now(),
-        "REJECTED", LocalDateTime.now()));
+        "REJECTED", LocalDateTime.now()
+    ));
+
+    public static final String TEST_STRING = "test";
+    public static final String ONE_TWO_THREE_FOUR = "1234";
+
+    private static final Map<String, ListType> LIST_TYPE_LOOKUP = Map.of(
+        "civilAndFamilyDailyCauseList.json", ListType.CIVIL_DAILY_CAUSE_LIST,
+        "civilDailyCauseList.json", ListType.CIVIL_DAILY_CAUSE_LIST,
+        "copDailyCauseList.json", ListType.COP_DAILY_CAUSE_LIST,
+        "familyDailyCauseList.json", ListType.FAMILY_DAILY_CAUSE_LIST,
+        "sjpPressMockJul22.json", ListType.SJP_PRESS_LIST,
+        "sjpPublicList.json", ListType.SJP_PUBLIC_LIST,
+        "sscsDailyList.json", ListType.SSCS_DAILY_LIST
+    );
 
     private String getInput(String resourcePath) throws IOException {
         try (InputStream inputStream = getClass().getResourceAsStream(resourcePath)) {
             return IOUtils.toString(inputStream, Charset.defaultCharset());
         }
+    }
+
+    private Artefact preBuiltArtefact(ListType listType) {
+        Artefact artefact = new Artefact();
+        artefact.setContentDate(LocalDateTime.now());
+        artefact.setListType(listType);
+        artefact.setProvenance(TEST_STRING);
+        artefact.setLanguage(Language.ENGLISH);
+        artefact.setLocationId(ONE_TWO_THREE_FOUR);
+        return artefact;
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"civilAndFamilyDailyCauseList.json", "civilDailyCauseList.json",
+        "copDailyCauseList.json", "familyDailyCauseList.json", "sjpPressMockJul22.json", "sjpPublicList.json",
+        "sscsDailyList.json"})
+    void testAllPdfListsAccessible(String filePath) throws IOException {
+        ListType listType = LIST_TYPE_LOOKUP.get(filePath);
+        Artefact artefact = preBuiltArtefact(listType);
+        Location location = new Location();
+        location.setName(TEST_STRING);
+        UUID uuid = UUID.randomUUID();
+        when(dataManagementService.getArtefactJsonBlob(uuid)).thenReturn(getInput("/mocks/" + filePath));
+        when(dataManagementService.getArtefact(uuid)).thenReturn(artefact);
+        when(dataManagementService.getLocation(ONE_TWO_THREE_FOUR)).thenReturn(location);
+        String htmlOutput = fileCreationService.jsonToHtml(uuid);
+        byte[] outputPdf = fileCreationService.generatePdfFromHtml(htmlOutput, true);
+        assertNotNull(outputPdf, "Output PDF is not a valid file. Did the generation process work?");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"civilAndFamilyDailyCauseList.json", "civilDailyCauseList.json",
+        "copDailyCauseList.json", "familyDailyCauseList.json", "sjpPressMockJul22.json", "sjpPublicList.json",
+        "sscsDailyList.json"})
+    void testAllPdfListsNonAccessible(String filePath) throws IOException {
+        ListType listType = LIST_TYPE_LOOKUP.get(filePath);
+        Artefact artefact = preBuiltArtefact(listType);
+        Location location = new Location();
+        location.setName(TEST_STRING);
+        UUID uuid = UUID.randomUUID();
+        when(dataManagementService.getArtefactJsonBlob(uuid)).thenReturn(getInput("/mocks/" + filePath));
+        when(dataManagementService.getArtefact(uuid)).thenReturn(artefact);
+        when(dataManagementService.getLocation(ONE_TWO_THREE_FOUR)).thenReturn(location);
+        String htmlOutput = fileCreationService.jsonToHtml(uuid);
+        byte[] outputPdf = fileCreationService.generatePdfFromHtml(htmlOutput, false);
+        assertNotNull(outputPdf, "Output PDF is not a valid file. Did the generation process work?");
     }
 
     @ParameterizedTest
@@ -70,11 +132,12 @@ class FileCreationServiceTest {
             PDFTextStripper pdfTextStripper = new PDFTextStripper();
 
             String outputText = pdfTextStripper.getText(doc);
-            log.info(outputText);
 
-            assertTrue(outputText.contains(
-                "An example file for the creation of PDF lists from our JSON artefact"),
-                       "Output pdf does not contain input text");
+            assertTrue(
+                outputText.contains(
+                    "An example file for the creation of PDF lists from our JSON artefact"),
+                "Output pdf does not contain input text"
+            );
 
             assertEquals(doc.getNumberOfPages(), 5, "Output pdf is not the correct length");
         } catch (Exception e) {
@@ -133,7 +196,9 @@ class FileCreationServiceTest {
 
     @Test
     void testCreateMediaApplicationReportingCsvSuccess() {
-        assertNotNull(fileCreationService.createMediaApplicationReportingCsv(MEDIA_APPLICATION_LIST),
-                      "Csv creation should not return null");
+        assertNotNull(
+            fileCreationService.createMediaApplicationReportingCsv(MEDIA_APPLICATION_LIST),
+            "Csv creation should not return null"
+        );
     }
 }
