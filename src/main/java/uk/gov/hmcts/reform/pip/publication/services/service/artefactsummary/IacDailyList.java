e84670bbc95b8371eace52010ea9657d08b41b59
ChrisS1512 <Christopher.Stacey@justice.gov.uk>
2022-10-14 16:28:16 +0100

    IAC PDF Template

---
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/artefactsummary/IacDailyList.java
@@ -0,0 +1,7 @@
+package uk.gov.hmcts.reform.pip.publication.services.service.artefactsummary;
+
+/**
+ * Summary class for the IAC Daily List that generates the summary in the email.
+ */
+public class IacDailyList {
+}
---
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/filegeneration/converters/IacDailyListConverter.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.converters;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import uk.gov.hmcts.reform.pip.publication.services.config.ThymeleafConfiguration;
+
+import java.io.IOException;
+import java.util.Map;
+
+/**
+ * Converter class for the IAC daily list to generate the PDF
+ */
+public class IacDailyListConverter implements Converter {
+
+    @Override
+    public String convert(JsonNode artefact, Map<String, String> metadata, Map<String, Object> language) throws IOException {
+        SpringTemplateEngine templateEngine = new ThymeleafConfiguration().templateEngine();
+
+        Context context = new Context();
+        context.setVariable("i18n", language);
+        context.setVariable("provenance", metadata.get("provenance"));
+        context.setVariable("artefact", artefact);
+        context.setVariable("telephone", artefact.get("venue").get("venueContact").get("venueTelephone"));
+        context.setVariable("email", artefact.get("venue").get("venueContact").get("venueEmail"));
+        return templateEngine.process("iacDailyList.html", context);
+    }
+
+    private void extractCourtLists() {
+
+    }
+
+}
