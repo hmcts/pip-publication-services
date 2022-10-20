01af158f314e1ac3822fddd9f3ae1fab0ea3bdf2
junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
2022-10-20 13:10:17 +0100

    remove code duplication

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/filegeneration/converters/PrimaryHealthListConverter.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/filegeneration/converters/PrimaryHealthListConverter.java
@@ -5,9 +5,6 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import uk.gov.hmcts.reform.pip.publication.services.config.ThymeleafConfiguration;
-import uk.gov.hmcts.reform.pip.publication.services.models.external.Language;
-import uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.helpers.DateHelper;
-import uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.helpers.LocationHelper;
 import uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.helpers.listmanipulation.TribunalNationalListsManipulation;
 
 import java.util.Map;
@@ -16,22 +13,8 @@ import java.util.Map;
 public class PrimaryHealthListConverter implements Converter {
     @Override
     public String convert(JsonNode artefact, Map<String, String> metadata, Map<String, Object> languageResources) {
-        Context context = new Context();
-        Language language = Language.valueOf(metadata.get("language"));
-        String publicationDate = artefact.get("document").get("publicationDate").asText();
-        context.setVariable("publicationDate", DateHelper.formatTimeStampToBst(publicationDate, language,
-                                                                               false, false));
-        context.setVariable("publicationTime", DateHelper.formatTimeStampToBst(publicationDate, language,
-                                                                               true, false));
-        context.setVariable("contentDate", metadata.get("contentDate"));
-        context.setVariable("provenance", metadata.get("provenance"));
-        context.setVariable("email", artefact.get("venue").get("venueContact").get("venueEmail").asText());
-        context.setVariable("i18n", languageResources);
-
-        LocationHelper.formatCourtAddress(artefact, "\n", true);
-
-        context.setVariable("cases", TribunalNationalListsManipulation.processRawListData(artefact, language));
-
+        Context context = TribunalNationalListsManipulation
+            .preprocessArtefactForTribunalNationalListsThymeLeafConverter(artefact, metadata, languageResources);
         SpringTemplateEngine templateEngine = new ThymeleafConfiguration().templateEngine();
         return templateEngine.process("primaryHealthList.html", context);
     }
--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/filegeneration/helpers/listmanipulation/TribunalNationalListsManipulation.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/filegeneration/helpers/listmanipulation/TribunalNationalListsManipulation.java
@@ -1,15 +1,38 @@
 package uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.helpers.listmanipulation;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import org.thymeleaf.context.Context;
 import uk.gov.hmcts.reform.pip.publication.services.models.external.Language;
 import uk.gov.hmcts.reform.pip.publication.services.models.templatemodels.TribunalNationalList;
 import uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.helpers.DateHelper;
+import uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.helpers.LocationHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public final class TribunalNationalListsManipulation {
     private TribunalNationalListsManipulation() {
+    }
+
+    public static Context preprocessArtefactForTribunalNationalListsThymeLeafConverter(
+        JsonNode artefact, Map<String, String> metadata, Map<String, Object> languageResources) {
+        Context context = new Context();
+        Language language = Language.valueOf(metadata.get("language"));
+        String publicationDate = artefact.get("document").get("publicationDate").asText();
+        context.setVariable("publicationDate", DateHelper.formatTimeStampToBst(publicationDate, language,
+                                                                               false, false));
+        context.setVariable("publicationTime", DateHelper.formatTimeStampToBst(publicationDate, language,
+                                                                               true, false));
+        context.setVariable("contentDate", metadata.get("contentDate"));
+        context.setVariable("provenance", metadata.get("provenance"));
+        context.setVariable("email", artefact.get("venue").get("venueContact").get("venueEmail").asText());
+        context.setVariable("i18n", languageResources);
+
+        LocationHelper.formatCourtAddress(artefact, "\n", true);
+
+        context.setVariable("cases", TribunalNationalListsManipulation.processRawListData(artefact, language));
+        return context;
     }
 
     public static List<TribunalNationalList> processRawListData(JsonNode data, Language language) {
