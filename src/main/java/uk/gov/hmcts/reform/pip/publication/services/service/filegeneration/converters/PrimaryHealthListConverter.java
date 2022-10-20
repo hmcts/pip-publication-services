86e28248811787e90f84da7de087351dbebcfe57
KianKwa <80688761+KianKwa@users.noreply.github.com>
2022-10-20 11:26:25 +0100

    PUB-1472 Add Primary Health tribunal PDF and artefact summary generation (#187)
    * Add Primary Health tribunal PDF and artefact summary generation
    
    * Corrected duration formatting for Welsh
    
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>

---
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/filegeneration/converters/PrimaryHealthListConverter.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.converters;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import uk.gov.hmcts.reform.pip.publication.services.config.ThymeleafConfiguration;
+import uk.gov.hmcts.reform.pip.publication.services.models.external.Language;
+import uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.helpers.DateHelper;
+import uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.helpers.LocationHelper;
+import uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.helpers.listmanipulation.PrimaryHealthListManipulation;
+
+import java.util.Map;
+
+@Service
+public class PrimaryHealthListConverter implements Converter {
+    @Override
+    public String convert(JsonNode artefact, Map<String, String> metadata, Map<String, Object> languageResources) {
+        Context context = new Context();
+        Language language = Language.valueOf(metadata.get("language"));
+        String publicationDate = artefact.get("document").get("publicationDate").asText();
+        context.setVariable("publicationDate", DateHelper.formatTimeStampToBst(publicationDate, language,
+                                                                               false, false));
+        context.setVariable("publicationTime", DateHelper.formatTimeStampToBst(publicationDate, language,
+                                                                               true, false));
+        context.setVariable("contentDate", metadata.get("contentDate"));
+        context.setVariable("provenance", metadata.get("provenance"));
+        context.setVariable("email", artefact.get("venue").get("venueContact").get("venueEmail").asText());
+        context.setVariable("i18n", languageResources);
+
+        LocationHelper.formatCourtAddress(artefact, "\n", true);
+        context.setVariable("cases", PrimaryHealthListManipulation.processRawListData(artefact, language));
+
+        SpringTemplateEngine templateEngine = new ThymeleafConfiguration().templateEngine();
+        return templateEngine.process("primaryHealthList.html", context);
+    }
+}
