7b998dc373c3d160eda5d04babd9830b6e47f5a2
KianKwa <kian.kwa@justice.gov.uk>
2022-12-23 13:24:15 +0000

    Checkstyle fix

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/controllers/NotificationController.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/controllers/NotificationController.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.pip.publication.services.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
