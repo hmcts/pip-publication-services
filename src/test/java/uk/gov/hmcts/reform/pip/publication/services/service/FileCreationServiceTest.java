e5b397adcb7d1c464091b5436fb47187bdae4dba
KianKwa <80688761+KianKwa@users.noreply.github.com>
2023-01-30 11:41:34 +0000

    PUB-1734 Media reporting slow response time (#250)
    * PUB-1678 Java 17 upgrade (#231)
    
    * Upgraded to java 17
    
    * Update to use new java features
    
    * Update to use new java features
    
    * Vulns
    
    Co-authored-by: KianKwa <kian.kwa@justice.gov.uk>
    
    * Updated suppressions (#232)
    
    * PUB-1751 Fix sonarCloud code smells (#236)
    
    * SonarCloud code smell fixes
    
    * Checkstyle fix
    
    * SonarCloud fixes
    
    * fix code smell
    
    * ignore code smell
    
    * fix build
    
    Co-authored-by: junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
    Co-authored-by: junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
    
    * Updated Chart (#248)
    
    * Improve create media application reporting response time
    
    ---------
    
    Co-authored-by: Josh <joshblackmoor@users.noreply.github.com>
    Co-authored-by: ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
    Co-authored-by: ChrisS1512 <Christopher.Stacey@justice.gov.uk>
    Co-authored-by: junaidiqbalmoj <junaid.iqbal@justice.gov.uk>
    Co-authored-by: junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>

--- src/test/java/uk/gov/hmcts/reform/pip/publication/services/service/FileCreationServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/publication/services/service/FileCreationServiceTest.java
@@ -10,12 +10,13 @@ import uk.gov.hmcts.reform.pip.publication.services.models.MediaApplication;
 import uk.gov.hmcts.reform.pip.publication.services.service.filegeneration.ExcelGenerationService;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -39,21 +40,37 @@ class FileCreationServiceTest {
 
     @InjectMocks
     private FileCreationService fileCreationService;
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
 
     private static final byte[] TEST_BYTE = "Test byte".getBytes();
+    private static final LocalDateTime REQUEST_DATE = LocalDateTime.now();
+    private static final LocalDateTime STATUS_DATE = LocalDateTime.now();
 
     private static final List<MediaApplication> MEDIA_APPLICATION_LIST = List.of(new MediaApplication(
         UUID.randomUUID(), "Test user", "test@email.com", "Test employer",
-        UUID.randomUUID().toString(), "test-image.png", LocalDateTime.now(),
-        "REJECTED", LocalDateTime.now()
+        UUID.randomUUID().toString(), "test-image.png", REQUEST_DATE,
+        "REJECTED", STATUS_DATE
     ));
+    private static final String EXPECTED_HEADER = "\"Full name\",\"Email\",\"Employer\",\"Request date\","
+        + "\"Status\",\"Status date\"";
+    private static final String EXPECTED_CONTENT = "\"Test user\",\"test@email.com\",\"Test employer\",\""
+        + REQUEST_DATE.format(DATE_TIME_FORMATTER) + "\",\"REJECTED\",\""
+        + STATUS_DATE.format(DATE_TIME_FORMATTER) + "\"";
 
     @Test
     void testCreateMediaApplicationReportingCsvSuccess() {
-        assertNotNull(
-            fileCreationService.createMediaApplicationReportingCsv(MEDIA_APPLICATION_LIST),
-            "Csv creation should not return null"
-        );
+        byte[] result = fileCreationService.createMediaApplicationReportingCsv(MEDIA_APPLICATION_LIST);
+        String[] csvResult = new String(result, StandardCharsets.UTF_8).split(System.lineSeparator());
+
+        assertThat(csvResult)
+            .as("CSV line count does not match")
+            .hasSize(2);
+        assertThat(csvResult[0])
+            .as("CSV header does not match")
+            .isEqualTo(EXPECTED_HEADER);
+        assertThat(csvResult[1])
+            .as("CSV content does not match")
+            .isEqualTo(EXPECTED_CONTENT);
     }
 
     @Test
