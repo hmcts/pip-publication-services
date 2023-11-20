2b52805b96e463a7299adb2a04127af2377da064
Kian Kwa <kian.kwa@justice.gov.uk>
2023-11-20 12:19:21 +0000

    Add validation flags to OTP email

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/models/request/OtpEmail.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/models/request/OtpEmail.java
@@ -1,11 +1,19 @@
 package uk.gov.hmcts.reform.pip.publication.services.models.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class OtpEmail {
+    @NotNull
+    @NotEmpty
     private String otp;
+
+    @NotNull
+    @Email
     private String email;
 }
