ec5ec8d5b04759e0fd65d932e91a10dfe983eb02
Kian Kwa <kian.kwa@justice.gov.uk>
2023-11-30 15:50:56 +0000

    Add more unit tests

--- src/test/java/uk/gov/hmcts/reform/pip/publication/services/service/RateLimitingServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/publication/services/service/RateLimitingServiceTest.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.pip.publication.services.service;
 
+import nl.altindag.log.LogCaptor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import uk.gov.hmcts.reform.pip.publication.services.errorhandling.exceptions.Too
 import uk.gov.hmcts.reform.pip.publication.services.notify.Templates;
 import uk.gov.hmcts.reform.pip.publication.services.utils.RedisConfigurationTestBase;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -22,19 +24,29 @@ class RateLimitingServiceTest extends RedisConfigurationTestBase {
     private static final String TEST_EMAIL2 = "test2@rateLimit.com";
     private static final String TEST_EMAIL3 = "test3@rateLimit.com";
     private static final String TEST_EMAIL4 = "test4@rateLimit.com";
+    private static final String TEST_EMAIL5 = "test5@rateLimit.com";
+    private static final String TEST_EMAIL6 = "test6@rateLimit.com";
 
     private static final Templates TEMPLATE_WITH_STANDARD_EMAIL_LIMIT = Templates.EXISTING_USER_WELCOME_EMAIL;
     private static final Templates TEMPLATE_WITH_HIGH_CAPACITY_EMAIL_LIMIT = Templates.SYSTEM_ADMIN_UPDATE_EMAIL;
+    private static final String STANDARD_CAPACITY_ERROR_MESSAGE = "Rate limit has been exceeded. Existing media "
+        + "account welcome email failed to be sent to t****@rateLimit.com";
+    private static final String HIGH_CAPACITY_ERROR_MESSAGE = "Rate limit has been exceeded. System admin "
+        + "notification email failed to be sent to t****@rateLimit.com";
 
-    private static final String ERROR_MESSAGE = "Rate limit has been exceeded.";
     private static final String EXCEPTION_MESSAGE = "Exception should be thrown";
     private static final String NO_EXCEPTION_MESSAGE = "Exception should not be thrown";
+    private static final String RESPONSE_TRUE_MESSAGE = "Method should return true";
+    private static final String RESPONSE_FALSE_MESSAGE = "Method should return false";
+    private static final String LOG_MESSAGE = "Error log does not match";
+
+    private LogCaptor logCaptor = LogCaptor.forClass(RateLimitingService.class);
 
     @Autowired
     private RateLimitingService rateLimitingService;
 
     @Test
-    void testExceptionIsThrownAfterReachingStandardEmailLimit() {
+    void testValidateThrowsExceptionAfterReachingStandardEmailLimit() {
         assertThatCode(() -> rateLimitingService.validate(TEST_EMAIL, TEMPLATE_WITH_STANDARD_EMAIL_LIMIT))
             .as(NO_EXCEPTION_MESSAGE)
             .doesNotThrowAnyException();
@@ -42,11 +54,11 @@ class RateLimitingServiceTest extends RedisConfigurationTestBase {
         assertThatThrownBy(() -> rateLimitingService.validate(TEST_EMAIL, TEMPLATE_WITH_STANDARD_EMAIL_LIMIT))
             .as(EXCEPTION_MESSAGE)
             .isInstanceOf(TooManyEmailsException.class)
-            .hasMessageContaining(ERROR_MESSAGE);
+            .hasMessageContaining(STANDARD_CAPACITY_ERROR_MESSAGE);
     }
 
     @Test
-    void testExceptionIsThrownAfterReachingHighCapacityEmailLimit() {
+    void testValidateThrowsExceptionAfterReachingHighCapacityEmailLimit() {
         assertThatCode(() -> rateLimitingService.validate(TEST_EMAIL, TEMPLATE_WITH_HIGH_CAPACITY_EMAIL_LIMIT))
             .as(NO_EXCEPTION_MESSAGE)
             .doesNotThrowAnyException();
@@ -58,11 +70,11 @@ class RateLimitingServiceTest extends RedisConfigurationTestBase {
         assertThatThrownBy(() -> rateLimitingService.validate(TEST_EMAIL, TEMPLATE_WITH_HIGH_CAPACITY_EMAIL_LIMIT))
             .as(EXCEPTION_MESSAGE)
             .isInstanceOf(TooManyEmailsException.class)
-            .hasMessageContaining(ERROR_MESSAGE);
+            .hasMessageContaining(HIGH_CAPACITY_ERROR_MESSAGE);
     }
 
     @Test
-    void testExceptionNotThrownUsingTheSameLimitIfEmailsDifferent() {
+    void testValidateThrowsExceptionUsingTheSameLimitIfEmailsDifferent() {
         assertThatCode(() -> rateLimitingService.validate(TEST_EMAIL2, TEMPLATE_WITH_STANDARD_EMAIL_LIMIT))
             .as(NO_EXCEPTION_MESSAGE)
             .doesNotThrowAnyException();
@@ -85,7 +97,7 @@ class RateLimitingServiceTest extends RedisConfigurationTestBase {
         assertThatThrownBy(() -> rateLimitingService.validate(TEST_EMAIL4, TEMPLATE_WITH_STANDARD_EMAIL_LIMIT))
             .as(EXCEPTION_MESSAGE)
             .isInstanceOf(TooManyEmailsException.class)
-            .hasMessageContaining(ERROR_MESSAGE);
+            .hasMessageContaining(STANDARD_CAPACITY_ERROR_MESSAGE);
 
         assertThatCode(() -> rateLimitingService.validate(TEST_EMAIL4, TEMPLATE_WITH_HIGH_CAPACITY_EMAIL_LIMIT))
             .as(NO_EXCEPTION_MESSAGE)
@@ -94,6 +106,48 @@ class RateLimitingServiceTest extends RedisConfigurationTestBase {
         assertThatThrownBy(() -> rateLimitingService.validate(TEST_EMAIL4, TEMPLATE_WITH_HIGH_CAPACITY_EMAIL_LIMIT))
             .as(EXCEPTION_MESSAGE)
             .isInstanceOf(TooManyEmailsException.class)
-            .hasMessageContaining(ERROR_MESSAGE);
+            .hasMessageContaining(HIGH_CAPACITY_ERROR_MESSAGE);
+    }
+
+    @Test
+    void testIsValidReturnsFalseAfterReachingStandardEmailLimit() {
+        assertThat(rateLimitingService.isValid(TEST_EMAIL5, TEMPLATE_WITH_STANDARD_EMAIL_LIMIT))
+            .as(RESPONSE_TRUE_MESSAGE)
+            .isTrue();
+
+        assertThat(rateLimitingService.isValid(TEST_EMAIL5, TEMPLATE_WITH_STANDARD_EMAIL_LIMIT))
+            .as(RESPONSE_FALSE_MESSAGE)
+            .isFalse();
+
+        assertThat(logCaptor.getErrorLogs())
+            .as(LOG_MESSAGE)
+            .isNotEmpty();
+
+        assertThat(logCaptor.getErrorLogs().get(0))
+            .as(LOG_MESSAGE)
+            .contains(STANDARD_CAPACITY_ERROR_MESSAGE);
+    }
+
+    @Test
+    void testIsValidReturnsFalseAfterReachingHighCapacityEmailLimit() {
+        assertThat(rateLimitingService.isValid(TEST_EMAIL6, TEMPLATE_WITH_HIGH_CAPACITY_EMAIL_LIMIT))
+            .as(RESPONSE_TRUE_MESSAGE)
+            .isTrue();
+
+        assertThat(rateLimitingService.isValid(TEST_EMAIL6, TEMPLATE_WITH_HIGH_CAPACITY_EMAIL_LIMIT))
+            .as(RESPONSE_TRUE_MESSAGE)
+            .isTrue();
+
+        assertThat(rateLimitingService.isValid(TEST_EMAIL6, TEMPLATE_WITH_HIGH_CAPACITY_EMAIL_LIMIT))
+            .as(RESPONSE_FALSE_MESSAGE)
+            .isFalse();
+
+        assertThat(logCaptor.getErrorLogs())
+            .as(LOG_MESSAGE)
+            .isNotEmpty();
+
+        assertThat(logCaptor.getErrorLogs().get(0))
+            .as(LOG_MESSAGE)
+            .contains(HIGH_CAPACITY_ERROR_MESSAGE);
     }
 }
