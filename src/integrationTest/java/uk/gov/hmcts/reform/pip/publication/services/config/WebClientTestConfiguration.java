d98a962fa59521e7a778b166067dfecd16d6fa37
ashwini.venkatesha <ashwini.venkatesha@justice.gov.uk>
2023-12-07 16:54:49 +0000

    Changed WebClientTestConfiguration.java

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/config/WebClientTestConfiguration.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/config/WebClientTestConfiguration.java
@@ -22,7 +22,7 @@ import reactor.netty.http.client.HttpClient;
 import javax.net.ssl.SSLException;
 
 @Configuration
-@Profile({"functional", "functional-rate-limit"})
+@Profile({"functional-rate-limit", "functional"})
 public class WebClientTestConfiguration {
 
     @Bean
