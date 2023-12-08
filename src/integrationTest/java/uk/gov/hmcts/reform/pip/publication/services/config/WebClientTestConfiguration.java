221e10740ad99fa2e9820458c20b7a271e6bbfe9
Kian Kwa <kian.kwa@justice.gov.uk>
2023-12-08 09:26:06 +0000

    Update functional test profile name

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/config/WebClientTestConfiguration.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/config/WebClientTestConfiguration.java
@@ -22,7 +22,7 @@ import reactor.netty.http.client.HttpClient;
 import javax.net.ssl.SSLException;
 
 @Configuration
-@Profile("functional | cub")
+@Profile("functional | functional-rate-limit")
 public class WebClientTestConfiguration {
 
     @Bean
