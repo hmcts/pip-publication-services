d455f16fde4afdfc221557e2de4a44ea76d0e9a5
KianKwa <80688761+KianKwa@users.noreply.github.com>
2023-12-14 09:19:59 +0000

    PUB-2193 Add APIM infrastructure for OTP endpoint (#393)
    * PUB-2193 Add publication services APIM for OTP (#385)
    
    * Add publication services APIM for OTP
    
    * Update APIM swagger json
    
    * Update infrastructure
    
    * Remove APIM logger module
    
    * Add new CVE suppression
    
    * Add new CVE suppression
    
    * Add new CVE suppression
    
    * Add new CVE suppression
    
    * Add suppression to dependencies
    
    * Update suppression
    
    * Update suppression
    
    * PUB-2193 Update APIM infrastructure (#387)
    
    * Add publication services APIM for OTP
    
    * Update APIM swagger json
    
    * Update infrastructure
    
    * Remove APIM logger module
    
    * Add new CVE suppression
    
    * Add new CVE suppression
    
    * Add new CVE suppression
    
    * Add new CVE suppression
    
    * Add suppression to dependencies
    
    * Update suppression
    
    * Update suppression
    
    * Update infrastructure
    
    * Add APIM logging (#388)
    
    * Deploy APIM to staging
    
    * Update APIM policies
    
    * deploy APIM to other non-prod env
    
    * Use new version of pip-data-models
    
    * Remove request/response body in APIM log

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,1 +1,13 @@
+locals {
+  env           = (var.env == "aat") ? "stg" : (var.env == "sandbox") ? "sbox" : "${(var.env == "perftest") ? "test" : "${var.env}"}"
+  env_long_name = var.env == "sbox" ? "sandbox" : var.env == "stg" ? "staging" : var.env
+  env_subdomain = local.env_long_name == "prod" ? "" : "${local.env_long_name}."
 
+  base_url      = "${var.product}-${var.component}.${local.env_subdomain}platform.hmcts.net"
+  prefix        = "${var.product}-ss"
+
+  apim_name     = "sds-api-mgmt-${local.env}"
+  apim_rg       = "ss-${local.env}-network-rg"
+
+  deploy_apim   = local.env == "stg" || local.env == "demo" || local.env == "test" || local.env == "sbox" || local.env == "prod" ? 1 : 0
+}
---
+++ infrastructure/tf-apim-logger.tf
@@ -0,0 +1,42 @@
+resource "azurerm_api_management_api_diagnostic" "api_logs" {
+  count                    = local.deploy_apim
+  identifier               = "applicationinsights"
+  resource_group_name      = local.apim_rg
+  api_management_name      = local.apim_name
+  api_name                 = local.apim_api_name
+  api_management_logger_id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${local.apim_rg}/providers/Microsoft.ApiManagement/service/${local.apim_name}/loggers/sds-api-mgmt-${local.env}-logger"
+
+  sampling_percentage       = 25.0
+  always_log_errors         = true
+  log_client_ip             = true
+  verbosity                 = "verbose"
+  http_correlation_protocol = "W3C"
+
+  frontend_request {
+    headers_to_log = [
+      "content-type",
+      "accept"
+    ]
+  }
+
+  frontend_response {
+    headers_to_log = [
+      "content-type",
+      "content-length"
+    ]
+  }
+
+  backend_request {
+    headers_to_log = [
+      "content-type",
+      "accept"
+    ]
+  }
+
+  backend_response {
+    headers_to_log = [
+      "content-type",
+      "content-length"
+    ]
+  }
+}
