0b740a5881c3927401271eae93ffeed6a441cc00
Kian Kwa <kian.kwa@justice.gov.uk>
2024-02-13 18:13:41 +0000

    Add security to Swagger API

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/config/OpenApiConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/config/OpenApiConfiguration.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.pip.publication.services.config;
 
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -7,7 +8,11 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static io.swagger.v3.oas.annotations.enums.SecuritySchemeIn.HEADER;
+import static io.swagger.v3.oas.annotations.enums.SecuritySchemeType.HTTP;
+
 @Configuration
+@SecurityScheme(name = "Bearer authentication", type = HTTP, scheme = "bearer", bearerFormat = "JWT", in = HEADER)
 public class OpenApiConfiguration {
 
     @Bean
--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/controllers/B2cNotificationController.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/controllers/B2cNotificationController.java
@@ -2,6 +2,8 @@ package uk.gov.hmcts.reform.pip.publication.services.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,7 @@ import uk.gov.hmcts.reform.pip.publication.services.service.UserNotificationServ
 @Tag(name = "Publication Services notification API for B2C")
 @RequestMapping("/notify")
 @IsB2C
+@SecurityRequirement(name = "Bearer authentication")
 public class B2cNotificationController {
     private final UserNotificationService userNotificationService;
 
@@ -28,6 +31,7 @@ public class B2cNotificationController {
 
     @ApiResponse(responseCode = "200", description = "OTP email successfully sent with referenceId: {Id}")
     @ApiResponse(responseCode = "400", description = "NotifyException error message")
+    @ApiResponse(responseCode = "401", description = "Invalid access credential")
     @ApiResponse(responseCode = "403", description = "User has not been authorized")
     @Operation(summary = "Send email containing B2C one-time password")
     @PostMapping("/otp")
