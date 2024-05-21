0f4a6e4d8a01571dcf040a7e6b81a674a8fb1916
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2024-05-21 17:02:14 +0100

    PUB-2471 - Updated javadoc + tests

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/models/request/SingleSubscriptionEmail.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/models/request/SingleSubscriptionEmail.java
@@ -1,17 +1,15 @@
 package uk.gov.hmcts.reform.pip.publication.services.models.request;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import uk.gov.hmcts.reform.pip.publication.services.validation.SubscriptionsConstraint;
 
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * This class is used for the deprecated endpoint when sending a single subscription email.
+ * This will be removed once migrated over to using the new endpoint.
+ */
 @Deprecated
 @Getter
 @Setter
