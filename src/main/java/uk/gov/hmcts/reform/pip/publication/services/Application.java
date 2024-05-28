629fed7c4aadcc7cb8f174d7d4b14d574a798e4b
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2024-05-28 11:27:20 +0100

    PUB-2471 - Added EnableAsync

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/Application.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/Application.java
@@ -2,8 +2,10 @@ package uk.gov.hmcts.reform.pip.publication.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
