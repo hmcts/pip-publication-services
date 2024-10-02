a71d6022d2ff318e9e31f9547a6a9bbf983e5ded
Kian Kwa <kian.kwa@justice.gov.uk>
2024-10-02 13:21:06 +0100

    Fixed functional tests

--- src/functionalTest/java/uk/gov/hmcts/pip/publication/services/NotificationEmailTests.java
+++ src/functionalTest/java/uk/gov/hmcts/pip/publication/services/NotificationEmailTests.java
@@ -11,8 +11,9 @@ import uk.gov.hmcts.pip.publication.services.utils.OAuthClient;
 import uk.gov.hmcts.reform.pip.publication.services.Application;
 import uk.gov.hmcts.reform.pip.publication.services.models.request.WelcomeEmail;
 
-import java.util.Collections;
+import java.util.Map;
 
+import static com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.http.HttpHeaders.AUTHORIZATION;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.http.HttpStatus.OK;
 
@@ -21,6 +22,8 @@ import static org.springframework.http.HttpStatus.OK;
 @SpringBootTest(classes = {Application.class, OAuthClient.class},
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class NotificationEmailTests extends FunctionalTestBase {
+    private static final String BEARER = "Bearer ";
+
     private static final String NOTIFY_URL = "/notify";
     private static final String MEDIA_WELCOME_EMAIL_URL = NOTIFY_URL + "/welcome-email";
 
@@ -31,7 +34,8 @@ class NotificationEmailTests extends FunctionalTestBase {
     void shouldSendMediaWelcomeEmailForNewUser() {
         WelcomeEmail requestBody = new WelcomeEmail(TEST_EMAIL, false, TEST_FULL_NAME);
 
-        final Response response = doPostRequest(MEDIA_WELCOME_EMAIL_URL, Collections.emptyMap(), requestBody);
+        final Response response = doPostRequest(MEDIA_WELCOME_EMAIL_URL, Map.of(AUTHORIZATION, BEARER + accessToken),
+                                                requestBody);
 
         assertThat(response.getStatusCode()).isEqualTo(OK.value());
     }
