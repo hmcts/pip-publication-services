ed573ed33905707d017394c63381947cfdc96095
Kian Kwa <kian.kwa@justice.gov.uk>
2024-11-20 13:49:26 +0000

    Create selected functional tests for notification emails

--- src/functionalTest/java/uk/gov/hmcts/pip/publication/services/utils/EmailNotificationClient.java
+++ src/functionalTest/java/uk/gov/hmcts/pip/publication/services/utils/EmailNotificationClient.java
@@ -7,6 +7,9 @@ import uk.gov.service.notify.NotificationClient;
 
 @Component
 public class EmailNotificationClient extends NotificationClient {
+    public static final String NOTIFICATION_STATUS = "delivered";
+    public static final String NOTIFICATION_TYPE = "email";
+
     @Autowired
     public EmailNotificationClient(@Value("${NOTIFY_API_KEY}") String apiKey) {
         super(apiKey);
