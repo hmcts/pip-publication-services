061a98b1aad0f2e2afefc19a02c17d79a122b973
Kian Kwa <kian.kwa@justice.gov.uk>
2025-01-03 13:51:45 +0000

    Further unit test simplification

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/client/EmailClientTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/client/EmailClientTest.java
@@ -1,24 +1,18 @@
 package uk.gov.hmcts.reform.pip.publication.services.client;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.reform.pip.publication.services.Application;
-import uk.gov.hmcts.reform.pip.publication.services.configuration.WebClientTestConfiguration;
 import uk.gov.hmcts.reform.pip.publication.services.utils.RedisConfigurationTestBase;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = {Application.class, WebClientTestConfiguration.class})
+@ActiveProfiles("integration")
+@SpringBootTest
 @DirtiesContext
-@SuppressWarnings("PMD.ImmutableField")
-@ActiveProfiles("test")
 class EmailClientTest extends RedisConfigurationTestBase {
 
     @Value("${notify.api.key}")
