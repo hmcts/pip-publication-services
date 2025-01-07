eb93fac859250665ecd6cd3d5f528f099831e3c8
Kian Kwa <kian.kwa@justice.gov.uk>
2025-01-07 15:50:32 +0000

    Fixed failing integration tests

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/config/SwaggerPublisherTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext
 @ActiveProfiles("integration")
 class SwaggerPublisherTest {
 
--- src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/controllers/GetWelcomeTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext
 @ActiveProfiles("integration")
 class GetWelcomeTest {
 
