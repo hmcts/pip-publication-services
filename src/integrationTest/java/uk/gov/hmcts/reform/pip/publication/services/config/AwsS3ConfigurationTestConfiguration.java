96741d745d077c8d2d62c345516217fd62df698a
junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>
2025-07-25 11:51:03 +0100

    mock s3client

--- src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/config/AwsS3ConfigurationTestConfiguration.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/pip/publication/services/config/AwsS3ConfigurationTestConfiguration.java
@@ -1,11 +1,16 @@
 package uk.gov.hmcts.reform.pip.publication.services.config;
 
-import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import software.amazon.awssdk.services.s3.S3Client;
 
+@Configuration
 @Profile({"integration", "integration-basic"})
 public class AwsS3ConfigurationTestConfiguration {
-    @Mock
-    private S3Client s3Client;
+    @Bean
+    public S3Client s3Client() {
+        return Mockito.mock(S3Client.class);
+    }
 }
