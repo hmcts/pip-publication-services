98fe8547bb339357883ddb5881d2b6cb01e38fec
kian.kwa <kian.kwa@justice.gov.uk>
2025-12-31 16:19:49 +0000

    Add JWT validation policy fragment to APIM policy files

---
+++ infrastructure/tf-apim-policy-fragment.tf
@@ -0,0 +1,7 @@
+resource "azurerm_api_management_policy_fragment" "jwt-validation" {
+  api_management_id = data.azurerm_api_management.sds_apim.id
+  name              = "${var.product}-jwt-validation"
+  format            = "rawxml"
+  description       = "This fragment validate input JWT token"
+  value             = file("${path.module}/resources/policy-fragments/jwt-validation-fragment.xml")
+}
