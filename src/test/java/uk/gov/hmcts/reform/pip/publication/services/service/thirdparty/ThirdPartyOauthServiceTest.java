16fba49be83ebfe9454203424887a0869852804c
kian.kwa <kian.kwa@justice.gov.uk>
2026-02-11 12:01:57 +0000

    Fix checkstyle issue

--- src/test/java/uk/gov/hmcts/reform/pip/publication/services/service/thirdparty/ThirdPartyOauthServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/pip/publication/services/service/thirdparty/ThirdPartyOauthServiceTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.reactive.function.client.WebClient;
 import uk.gov.hmcts.reform.pip.model.thirdparty.ThirdPartyOauthConfiguration;
