fcf7723493b953cb6a353b0d4d520d01e4e27605
KianKwa <kian.kwa@justice.gov.uk>
2026-06-22 12:31:23 +0100

    Fix checkstyle

--- src/functionalTest/java/uk/gov/hmcts/pip/publication/services/SubscriptionNotificationEmailTests.java
+++ src/functionalTest/java/uk/gov/hmcts/pip/publication/services/SubscriptionNotificationEmailTests.java
@@ -6,7 +6,6 @@ import org.assertj.core.api.AssertionsForClassTypes;
 import org.awaitility.Awaitility;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
