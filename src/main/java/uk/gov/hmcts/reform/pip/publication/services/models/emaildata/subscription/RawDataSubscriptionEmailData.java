7f1d56f57c4bee180b4754c1220f6c202766bc8c
Rebecca <67150587+RebeccaHayleyPickles@users.noreply.github.com>
2026-07-15 11:41:00 +0100

    PUB-3260 Remove csv file subscription emails (#600)
    * Remove csv file subscription emails
    
    * Update RawDataSubscriptionEmailGenerator.java
    
    * Update suppressions.xml
    
    * Update data models package
    
    * Update suppressions.xml
    
    * update max in memory size
    
    * Update suppressions.xml
    
    * update pip data model version
    
    * fix build
    
    ---------
    
    Co-authored-by: junaidiqbalmoj <84805836+junaidiqbalmoj@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/models/emaildata/subscription/RawDataSubscriptionEmailData.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/models/emaildata/subscription/RawDataSubscriptionEmailData.java
@@ -24,11 +24,10 @@ public class RawDataSubscriptionEmailData extends EmailData {
     private String artefactSummary;
     private byte[] pdf;
     private byte[] excel;
-    private byte[] csv;
     private RetentionPeriodDuration fileRetentionWeeks;
 
     public RawDataSubscriptionEmailData(SubscriptionEmail subscriptionEmail, Artefact artefact, String artefactSummary,
-                                        byte[] pdf, byte[] excel, byte[] csv, String locationName,
+                                        byte[] pdf, byte[] excel, String locationName,
                                         int fileRetentionWeeks, String referenceId) {
         super(subscriptionEmail.getEmail(), referenceId);
         this.subscriptions = subscriptionEmail.getSubscriptions();
@@ -37,7 +36,6 @@ public class RawDataSubscriptionEmailData extends EmailData {
         this.artefactSummary = artefactSummary;
         this.pdf = pdf == null ? new byte[0] : Arrays.copyOf(pdf, pdf.length);
         this.excel = excel == null ? new byte[0] : Arrays.copyOf(excel, excel.length);
-        this.csv = csv == null ? new byte[0] : Arrays.copyOf(csv, csv.length);
         this.fileRetentionWeeks = new RetentionPeriodDuration(fileRetentionWeeks, ChronoUnit.WEEKS);
     }
 }
