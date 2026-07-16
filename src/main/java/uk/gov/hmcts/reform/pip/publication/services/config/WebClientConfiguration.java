071286be38839e816379695ca30a94425cec4b23
Daniel Alao <Daniel.Alao@justice.gov.uk>
2026-07-16 09:21:12 +0100

    fix functional test issue

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/config/WebClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/config/WebClientConfiguration.java
@@ -14,8 +14,12 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
@@ -59,12 +63,23 @@ public class WebClientConfiguration {
 
     @Bean
     @Profile("!dev")
+    // required to fix known issue: https://github.com/spring-projects/spring-security/issues/19324
     public WebClient webClient(OAuth2AuthorizedClientManager authorizedClientManager) {
-        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2Client =
-            new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
-        oauth2Client.setDefaultClientRegistrationId("dataManagementApi");
-        return WebClient.builder().exchangeStrategies(STRATEGIES)
-            .apply(oauth2Client.oauth2Configuration()).build();
+        ExchangeFilterFunction oauth2Client = (request, next) -> {
+            OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+                .withClientRegistrationId("dataManagementApi")
+                .principal("dataManagementApi")
+                .build();
+            OAuth2AuthorizedClient authorizedClient = authorizedClientManager.authorize(authorizeRequest);
+            ClientRequest authorizedRequest = ClientRequest.from(request)
+                .headers(headers -> headers.setBearerAuth(authorizedClient.getAccessToken().getTokenValue()))
+                .build();
+            return next.exchange(authorizedRequest);
+        };
+        return WebClient.builder()
+            .exchangeStrategies(STRATEGIES)
+            .filter(oauth2Client)
+            .build();
     }
 
     /**
