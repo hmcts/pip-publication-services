b9abc0b5a4f7d43546b5c5fe5572552e26d3eabc
KianKwa <kian.kwa@justice.gov.uk>
2026-09-21 12:41:57 +0100

    Use jackson 3 JsonNode lib

--- src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/thirdparty/ThirdPartyOauthService.java
+++ src/main/java/uk/gov/hmcts/reform/pip/publication/services/service/thirdparty/ThirdPartyOauthService.java
@@ -1,6 +1,6 @@
 package uk.gov.hmcts.reform.pip.publication.services.service.thirdparty;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import tools.jackson.databind.JsonNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
